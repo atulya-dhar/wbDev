5761d8f313a210de1f10f46712b7a76cf5985010
atulya-dhar <atulyadhar0428@gmail.com>
2022-11-01 21:51:38 +0530

    first-commit

---
+++ dom.js
@@ -0,0 +1,25 @@
+// EXAMINE THE DOCUMENT OBJECT //
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+// console.log(document.form[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID //
+// console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+//headerTitle.textContent = 'The Herald'
+//headerTitle.innerText = 'Goodbye';
+//console.log(headerTitle.innerText);
+headerTitle.style.borderBottom = 'solid 3px #000';
