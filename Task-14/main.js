5a1ac3c3e91bb23eeff3f8b3b8e2a6f3c113c252
Atulya Dhar <atulyadhar0428@gmail.com>
2023-10-08 06:50:22 +0530

    Task-14

---
+++ Task-14/main.js
@@ -0,0 +1,99 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(addUser) {
+    addUser.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('Error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+        // Creating li element
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} :${emailInput.value}`));
+        username = nameInput.value;
+        useremail = emailInput.value;
+
+        // Create Edit Button
+        var editBtn = document.createElement('button');
+
+        // Add class to edit button
+        editBtn.className = 'btn btn-edit btn-sm float-right edit';
+
+        // Append textNode
+        editBtn.appendChild(document.createTextNode('Edit'));
+
+        // Append edit button to li
+        li.appendChild(editBtn);
+        
+        // Creating Delete Button
+        var deleteBtn = document.createElement('button');
+
+        // Add class to delete button
+        deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+        // Append textNode
+        deleteBtn.appendChild(document.createTextNode('X'));
+
+        // Append delete button to li
+        li.appendChild(deleteBtn);
+
+        // Appending li to list
+        userList.appendChild(li);
+        // Storing Data
+        //var name = localStorage.setItem('username', username);
+        //var email = localStorage.setItem('useremail', useremail);
+        const obj = {
+            username,
+            useremail,
+        }
+        localStorage.setItem(obj.useremail, JSON.stringify(obj));
+
+
+        // Retrieving Data
+        //var name = localStorage.getItem('username', username);
+        //var email = localStorage.getItem('useremail', useremail);
+
+        // Remove item
+        // function deleteItem(addUser){
+        //     if(addUser.target.classList.contains('delete')) {
+        //         if(confirm('Are you sure?')) {
+        //             var li = addUser.target.parentElement;
+        //             localStorage.removeItem(obj.useremail)
+        //             itemList.removeChild(li);
+        //         }
+        //     }
+        // }
+
+        deleteBtn.onclick = () => {
+            localStorage.removeItem(obj.useremail);
+            userList.removeChild(li);
+        }
+
+        li.appendChild(deleteBtn);
+        userList.appendChild(li);
+
+        // Edit item
+        editBtn.onclick = () => {
+            localStorage.removeItem(obj.useremail);
+            userList.removeChild(li);
+            document.getElementById('name').value = obj.username;
+            document.getElementById('email').value = obj.useremail;
+        }
+
+        li.appendChild(editBtn);
+        userList.appendChild(li);
+
+
+        // Clear field
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
